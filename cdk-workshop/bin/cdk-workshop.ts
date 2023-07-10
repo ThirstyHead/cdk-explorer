63b9fbe81cdf79d5e5d5d8dbf992fae62ee73b30
Scott Davis <scott@thirstyhead.com>
2023-07-10 10:33:43 -0600

    adding this back in to fully undeploy

--- cdk-workshop/bin/cdk-workshop.ts
+++ cdk-workshop/bin/cdk-workshop.ts
@@ -2,5 +2,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
-// const app = new cdk.App();
-// new CdkWorkshopStack(app, 'CdkWorkshopStack');
+const app = new cdk.App();
+new CdkWorkshopStack(app, 'CdkWorkshopStack');
