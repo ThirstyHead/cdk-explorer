01cbbbccb56584ad8c81a288f386a2d98d81657c
Scott Davis <scott@thirstyhead.com>
2023-07-10 10:30:10 -0600

    pulling down the stack after auth errors

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,15 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
-    });
+    // const hello = new lambda.Function(this, 'HelloHandler', {
+    //   runtime: lambda.Runtime.NODEJS_16_X,
+    //   code: lambda.Code.fromAsset('lambda'),
+    //   handler: 'hello.handler'
+    // });
+
+    // const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+    //   downstream: hello
+    // });
+
+    // defines an API Gateway API resource backed by 'hello' function
+    // new apigw.LambdaRestApi(this, 'Endpoint', {
+    //   handler: helloWithCounter.handler
+    // });
+
+    // hello.addPermission('PermitAPIGInvocation', {
+    //   principal: new ServicePrincipal('apigateway.amazonaws.com'),
+    //   //sourceArn: apigw.arnForExecuteApi('*')
+    //   sourceArn: apigw.
+    // });
   }
 }
---
+++ cdk-workshop/lib/hitcounter.ts
@@ -0,0 +1,43 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import {Construct} from 'constructs';
+import { ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+
+export interface HitCounterProps{
+    downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct{
+    /** allows accessing the counter function */
+    public readonly handler: lambda.Function;
+
+    constructor(scope: Construct, id: string, props: HitCounterProps){
+        super(scope, id);
+
+        const table = new dynamodb.Table(this, 'Hits', {
+            partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING}
+        });
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment: {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: table.tableName
+            }
+        });
+
+        // grant the lambda role read/write permissions to our table
+        table.grantReadWriteData(this.handler);
+    
+    //    this.handler.grantInvoke(taskDefinitionAdmin.taskRole)
+
+
+        // this.handler.addPermission('PermitAPIGInvocation', {
+        //     principal: new ServicePrincipal('apigateway.amazonaws.com'),
+        //     sourceArn: apigw.arnForExecuteApi('*')
+        //   });
+    }
+}
